0f26f2a91f95fe0d1aa5db369d89e71ca869b0b2
Cloydi <95918602+Cloydi@users.noreply.github.com>
2022-06-08 22:48:45 +0800

    Update Database.php

--- Database.php
+++ Database.php
@@ -82,7 +82,7 @@
 </head>
 <body>
 <?php
-    $email = $password = $comment ="";
+    $email = $name = $comment ="";
     if($_SERVER["REQUEST_METHOD"] == "POST")
     {
         $email      =   test_input($_POST["email"]);
